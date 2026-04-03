9652b376bf9ebbd2d0a3a2b9e6e74faad8ce1df6
param-punjab <matharooparam@gmail.com>
2026-04-03 23:28:49 +0530

    finally I created graph from scratch it's inside './work/data_structure/graph/t2'

--- work/data_structure/graph/t2/main.cpp
+++ work/data_structure/graph/t2/main.cpp
@@ -2,14 +2,42 @@
 #include <vector>
 using namespace std;
 
-class Graph() {
-    int data;
+class graph {
+public:
+  vector<vector<int>> g;
+  int nodes;
 
-    Grpah();
+  graph(int n) {
+    nodes = n;
+    g.resize(nodes);
+    cout << "Graph got created with nodes: " << 0 << " - " << nodes - 1 << endl;
+  }
+
+  void connection(int s, int d) {
+    g[s].push_back(d);
+    g[d].push_back(s);
+    cout << "Connection got established between nodes: " << s << " and " << d << endl;
+  }
+
+  void print() {
+    int size;
+    cout << "Displaying Graph" << endl;
+    for (int i = 0; i < nodes; i++) {
+        size = g[i].size();
+        cout << i << " -> "; 
+        for (int j = 0; j < size; j++) {
+            cout << g[i][j] << ", "; 
+        } 
+        cout << endl;
+    }
+  }
 };
 
 int main() {
-    vector<vector<int>> adj;
-
-    cout << "Graph" << endl;
+  cout << " --- Graph Implementation --- " << endl;
+  graph g1(3);
+  g1.connection(0, 1);
+  g1.connection(0, 2);
+  g1.print();
+  return 0;
 }
