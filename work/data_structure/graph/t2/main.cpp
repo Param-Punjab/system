b218e57ba9b09695726996d354382ebc1aa4f16b
param-punjab <matharooparam@gmail.com>
2026-04-03 14:37:44 +0530

    live report log 03-04-2026 14:32

---
+++ work/data_structure/graph/t2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Graph() {
+    int data;
+
+    Grpah();
+};
+
+int main() {
+    vector<vector<int>> adj;
+
+    cout << "Graph" << endl;
+}
