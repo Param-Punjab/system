350afa8eca002651e885e2c3a1ee44914acb1d9b
param-punjab <matharooparam@gmail.com>
2026-04-03 23:59:36 +0530

    BFS implementation with bugs

--- work/algo/BFS/main.cpp
+++ work/algo/BFS/main.cpp
@@ -11,14 +11,17 @@ public:
     Graph(int n) {
         nodes = n;
         adj.resize(n);
+        cout << "Graph got created" << endl;
     }
 
     void addEdge(int u, int v) {
         adj[u].push_back(v);
         adj[v].push_back(u);
+        cout << "Edge got connected" << endl;
     }
 
     void print_graph() {
+        cout << "Printing Grpah" << endl;
         for (int i = 0; i < adj.size(); i++) {
             cout << i << " -> ";
             for (int j = 0; j < adj[i].size(); j++) {
@@ -27,16 +30,31 @@ public:
             cout << endl;
         }
     }
+
+    void BFS() {
+        queue<int> que;
+        int s = 0;
+        for (int j = 0; j < nodes; j++) {
+            if (!que.empty()) {
+                s = que.front();
+                que.pop();
+            }
+            int size = adj[s].size();
+            for (int i = 0; i < size; i++) {
+                cout << adj[s][i] << " ";
+                que.push(adj[s][i]);
+            }
+        }
+    }
 };
 
 int main() {
     vector<vector<int>> adj;
-    int n;
-    int s;
-
-    queue<int> q;
-    vector<bool> used(n);
-    vector<int> d(n), p(n);
+    Graph g(3);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.print_graph();
+    g.BFS();
 
     return 0;
 }
