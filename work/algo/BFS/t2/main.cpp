582474f098872ae9db146f11089af4cbd0075fa6
param-punjab <mathrooparam@gmail.com>
2026-04-05 14:18:32 +0530

    I did that Breadth First Search '/system/work/algo/BFS/t2/main.cpp' through long period

--- work/algo/BFS/t2/main.cpp
+++ work/algo/BFS/t2/main.cpp
@@ -35,32 +35,69 @@ public:
     }
   }
 
+// Commented by Param 05-04-2026
+//  void BFS() {
+//    queue<int> que;
+//    int s = 0;
+//    bool visited_arr[nodes] = {0};
+//    for (int i = 0; i < nodes; i++) {
+//      if (!que.empty()) {
+//        s = que.front();
+//        que.pop();
+//      }
+//      for (int j = 0; j < g.size(); j++) {
+//        if (visited_arr[s] == false && 0) {
+//          cout << g[i][s] << " ";
+//          cout << g[i][j] << " ";
+//          que.push(g[i][s]);
+//          visited_arr[g[i][s]] = 1;
+//        }
+//      }
+//    }
+//  }
+
+
   void BFS() {
+    // Clean BFS mental model
+    // 1. Push start node
+    // 2. while queue not empty
+    // 3. Pop
+    // 4. If not visited -> process + mark
+    // 5. Push only unvisited neighbors
+    cout << endl << " BFS Printing " << endl;
     queue<int> que;
     int s = 0;
-    bool visited_arr[nodes] = {0};
-    for (int i = 0; i < nodes; i++) {
-      if (!que.empty()) {
-        s = que.front();
-        que.pop();
-      }
-      for (int j = 0; j < g.size(); j++) {
-        if (visited_arr[s] == false && 0) {
-          cout << g[i][s] << " ";
-          cout << g[i][j] << " ";
-          que.push(g[i][s]);
-          visited_arr[g[i][s]] = 1;
+    que.push(s);
+    vector<bool> arr(nodes, false);
+    while (!que.empty()) {
+      s = que.front();
+      que.pop();
+      if (arr[s] != true) {
+        // Print node
+        cout << s << " ";
+        // Visited[node] = True
+        arr[s] = true;
+        // Node naoubher added into queue
+        for (int i = 0; i < g[s].size(); i++) {
+          if (!arr[g[s][i]]) {que.push(g[s][i]);}
+
         }
       }
     }
   }
+
 };
 
 int main() {
   cout << " --- Graph Implementation --- " << endl;
-  graph g1(3);
+  graph g1(7);
   g1.connection(0, 1);
   g1.connection(0, 2);
+  g1.connection(1, 3);
+  g1.connection(1, 4);
+  g1.connection(2, 5);
+  g1.connection(2, 6);
   g1.print();
+  g1.BFS();
   return 0;
 }
